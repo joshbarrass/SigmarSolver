e613e36033b324d33288dcfd79c46d35102a24a5
Joshua Barrass <josh.barrass.work@gmail.com>
2026-05-16 16:16:53 +0100

    Ensure sigmarsolver.h is completely self-contained
    Basically copy in the contents of dllexport.h -- if all you're using
    is the C API, this makes it much simpler to use the library, since you
    will only need this one header.

--- include/sigmarsolver.h
+++ include/sigmarsolver.h
@@ -1,6 +1,13 @@
 #ifndef SIGMARSOLVER_H
 #define SIGMARSOLVER_H
-#include "dllexport.h"
+#ifndef _DLL_EXPORT_H
+#define _DLL_EXPORT_H
+#ifdef _MSC_VER
+  #define EXPORT __declspec(dllexport)
+#else
+  #define EXPORT
+#endif
+#endif
 #ifdef __cplusplus
 extern "C" {
 #endif
